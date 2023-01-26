af6bb348ea73acb784099a3203364c80776f14ef
Matthias Blank <Matthias.Blank2@gmx.net>
2023-01-26 19:52:39 +0100

    Javascript und Typescript Tutorial

---
+++ src/app/ts/TutorialTypescript.ts
@@ -0,0 +1,57 @@
+interface Rectangle {
+  height: number;
+  width: number;
+}
+
+interface ColoredRectangle extends Rectangle {
+  color: string;
+}
+
+const coloredRectangle: ColoredRectangle = {
+  height: 20,
+  width: 10,
+  color: 'red'
+};
+
+/*
+const user: User = {
+  name: "Hayes",
+  id: 0,
+};
+*/
+
+type MyBool2 = true | false;
+const one: MyBool2 = true;
+console.log(one);
+
+type PositiveOddNumbersUnderTen = 1 | 3 | 5 | 7 | 9;
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+// tslint:disable-next-line:typedef
+function logPoint(p: Point) {
+  console.log(`${p.x}, ${p.y}`);
+}
+
+// logs "12, 26"
+const point = {x: 12, y: 26};
+logPoint(point);
+
+function flipCoin(): boolean {
+  // Meant to be Math.random()
+  return Math.random() < 0.5;
+}
+
+function greet(name: string): void {
+  console.log("Hello, " + name.toUpperCase() + "!!");
+}
+
+type MyBool = true | false;
+
+
+function getTime(): number {
+  return new Date().getTime();
+}
